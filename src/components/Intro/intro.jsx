69853930b5ebdd190240138a30ef861ce746f8e5
Julian Renzetti <julian.renzetti@outlook.com>
2026-05-05 16:10:41 -0700

    fix typo

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -17,7 +17,7 @@ const Intro = () => {
             👋
           </span>        </h1>
         <p className="description">
-          I'm a former business student and commercial banker who discovered a passion for software. I'm currently in my final semester at the University of British Columbia in the Bachelor of Computer Science second degree program. I have a year of technical internships under my belt with experience in Android development, backend systems, query optimizaton, and Python scripting. I'm currently looking for full time opportunities starting in September 2026.
+          I'm a former business student and commercial banker who discovered a passion for software. I'm currently in my final semester at the University of British Columbia in the Bachelor of Computer Science second degree program. I have a year of technical internships under my belt with experience in Android development, backend systems, query optimization, and Python scripting. I'm currently looking for full time opportunities starting in September 2026.
           <br />
           <br />
           My hobbies include playing basketball, running, snowboarding, and beach volleyball. I love to travel, although being a student has put travel plans on hold for the time being. I've also gotten into gardening and am always looking into ways to optimize my patio farm.
